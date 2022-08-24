a84158a1a0ab65f23cb00e59e49a529c15370dbc
priKSouza <priscilakarinajump@gmail.com>
2022-08-24 13:31:45 -0300

    Estilizando

--- src/pages/Sucesso/Sucesso.jsx
+++ src/pages/Sucesso/Sucesso.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import S from "./Sucesso.module.css";
+import { FiUserCheck } from "react-icons/fi"
 
 const Success = () => {
 
   return (
     <div className={S.container}>
-      <h3>Usuário cadastrado com sucesso!</h3>
+      <h3 className={S.sucesso}>Cadastro finalizado!</h3>
+      <FiUserCheck size="40px" color="#FFF000"/>
     </div>
   );
 };
